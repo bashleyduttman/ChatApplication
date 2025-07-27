8d2a4b98458e3b5a331287015766fb6c9bf8cd4f
bashleyduttman <bashleyduttman@gmail.com>
2025-07-27 15:26:33 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,10 +19,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://chat-application-kappa-fawn.vercel.app"
+    ],
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
